acf875f3412617c6b3ffb98172a3f9fbeee54f71
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2024-02-28 15:59:18 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+    private TextView tvDetail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        tvDetail = findViewById(R.id.tv_detail);
+        Intent receivedIntent = getIntent();
+
+        if (receivedIntent != null) {
+            String data = receivedIntent.getStringExtra("operator");
+            tvDetail.setText(data);
+        }
+    }
+}
